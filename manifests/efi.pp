a1c76c440f9995ba07aec0402263a7604ac70cb5
mkr <matthias.krause@vitadisplays.com>
2014-01-19 21:07:19 +0100

    removing unused files

--- manifests/efi.pp
+++
@@ -1,9 +0,0 @@
-define oxid::efiRemover (
-  $db,
-  $host = $oxid::mysql::params::default_host,
-  $port = $oxid::mysql::params::default_port,
-  $user = $oxid::mysql::params::default_user,
-  $password) {
-  $tables_to_drop = ["efi_paypal_transaction_drop"]
-
-}
--- manifests/stages.pp
+++
@@ -1,8 +0,0 @@
-class oxid::stages {
-  stage { 'pre': before => Stage['main'] }
-
-  stage { 'system':
-    require => Stage['pre'],
-    before  => Stage['main']
-  }
-}
