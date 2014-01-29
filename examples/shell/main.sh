5a233815b3f7987425f463eee6c27d472ab9b54d
mkr <matthias.krause@vitadisplays.com>
2014-01-29 08:48:55 +0100

    bugfix update-alternatives commands from --config to --auto

--- examples/shell/main.sh
+++ examples/shell/main.sh
@@ -40,8 +40,8 @@ if [ -x $APT_GET ]; then
      
      apt-get -q -y install ruby1.9.1 ruby1.9.1-dev rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev libaugeas-ruby1.9.1
      update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz  /usr/share/man/man1/ruby1.9.1.1.gz --slave /usr/bin/ri ri /usr/bin/ri1.9.1 --slave /usr/bin/irb irb /usr/bin/irb1.9.1 --slave /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
-     update-alternatives --config ruby
-     update-alternatives --config gem
+     update-alternatives --auto ruby
+     update-alternatives --auto gem
 else
 	 echo "No package installer available. You may need to install ruby and gem manually."    
 fi
