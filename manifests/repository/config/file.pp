47cc690e9d5bbfc819ff474a56a38024cb7bf9f4
mkr <matthias.krause@vitadisplays.com>
2014-03-20 17:05:41 +0100

    remove ordering

--- manifests/repository/config/file.pp
+++ manifests/repository/config/file.pp
@@ -1,11 +1,6 @@
 include oxid::params
 
 define oxid::repository::config::file ($root = "") {
-
-  if defined(Oxid::Update) {
-    Oxid::Update <| |> -> Oxid::Module[$name]
-  }
-
   $oxid::params::repository_configs["${name}"] = {
     'type'      => 'oxid::repository::file',
     'directory' => $root
